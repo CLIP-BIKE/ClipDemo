ff6a423ce0149643d15935b94b257eb7b29926d4
woodley <gelinw3@gmail.com>
2023-03-04 10:39:14 -0500

    wrap DeviceProvider to App so that all the application can acces connectedDevice

--- App.tsx
+++ App.tsx
@@ -9,19 +9,17 @@ import React from 'react';
 import Main from './Screens/Main';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
 import { useColorScheme, View } from 'react-native';
-import CheckForBluetoothPermissions from './BLE components/CheckForPermissions';
-import UseBLE from './BLE components/UseBLE';
-import Info from './Screens/Info';
+import { DeviceProvider } from './BLE components/DeviceContext';
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-  //CheckForBluetoothPermissions();
-  //UseBLE();
   return (
-    <Main></Main>
+    <DeviceProvider>
+      <Main></Main>
+    </DeviceProvider>
     );
 }
 export default App;
