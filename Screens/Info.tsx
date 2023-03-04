31c0a8828be7fe555e96c7ee2febfcc869e0634a
woodley <gelinw3@gmail.com>
2023-03-04 10:34:49 -0500

    declare state from device Context

--- Screens/Info.tsx
+++ Screens/Info.tsx
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
-import { Image, KeyboardAvoidingView, Platform, SafeAreaView, ScrollView, Text, Touchable, TouchableOpacity, View } from 'react-native';
+import React, { useContext, useEffect, useLayoutEffect, useState } from 'react';
+import {ScrollView, Text,TouchableOpacity, View } from 'react-native';
 import { LogBox } from 'react-native';
 import { TextInput } from 'react-native';
-import useBLE from '../BLE components/UseBLE';
+import { DeviceContext } from '../BLE components/DeviceContext';
+import useBLE from '../BLE components/useBLE';
 
 LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
 
 function Info() {
-  const {
-    telemetry,
-    FWVer
-  } = useBLE();
-  const [command, setCommand] = useState<string>('');
-  const onChangeText = (command: string) =>{
-    setCommand(command)
-  }
+  const { state, dispatch } = useContext(DeviceContext);
     return (
       <View style = {{ backgroundColor: 'grey', height: '100%', flex: 1, padding: 5}}>
         <ScrollView style = {{height: '85%', backgroundColor: 'white'}}>
+          {state.connectedDevice? <Text>{state.connectedDevice?.id} is connected</Text>:<Text>Please Connect to a device</Text>}
         </ScrollView>
         <View style = {{ backgroundColor: 'white', padding: 5, flexDirection: 'row', alignItems: 'center', position: 'relative', top:'1%'}}>
-          <TextInput style ={{borderWidth: 1, borderColor: '#ccc', borderRadius: 5, padding: 10, width: '80%'}} placeholder='Enter Command' value={command} onChangeText = {setCommand}></TextInput>
+          <TextInput style ={{borderWidth: 1, borderColor: '#ccc', borderRadius: 5, padding: 10, width: '80%'}} placeholder='Enter Command'></TextInput>
           <TouchableOpacity style={{backgroundColor: '#007AFF', padding: 10, borderRadius: 5, marginLeft: 10}}>
           < Text style={{ color: 'blue', fontSize: 16}}>Send</Text>
           </TouchableOpacity>
