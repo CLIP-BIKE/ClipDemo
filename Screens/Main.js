664193af4ea5700874c7a5143345662cae106b56
woodley <gelinw3@gmail.com>
2023-03-05 16:15:05 -0500

    remove keyboardAvoidView

--- Screens/Main.js
+++ Screens/Main.js
@@ -2,14 +2,12 @@ import * as React from 'react';
 import { View, Text,Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { KeyboardAvoidingView } from 'react-native';
 import Scan from './Scan';
 import Info from './Info';
 
 const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
-    <KeyboardAvoidingView style={{ flex: 1 }} behavior="height">
       <Tab.Navigator screenOptions = {{tabBarShowLabel: false, tabBarStyle: {height: 50, margin: 1}}}>
       <Tab.Screen name = "Scanner" component = {Scan}  options = {{
         tabBarIcon: ({focused}) =>(
@@ -21,16 +19,13 @@ function MyTabs() {
       }}></Tab.Screen>
       <Tab.Screen name = "Device info" component={Info} options = {{
         tabBarIcon: ({focused}) =>(
-            <KeyboardAvoidingView behavior='padding'>
-              <View style = {{alignItems:'center'}}>
+            <View style = {{alignItems:'center'}}>
               <Image source={require('../assets/info.png')} resizeMode = "contain" style = {{width: 30, height: 25, tintColor: focused? '#5abf90' : '#748c94'}}></Image>
               <Text style = {{color: focused? '#5abf90' : '#748c94'}}>Device info!</Text>
             </View>
-            </KeyboardAvoidingView>
         ),
       }}></Tab.Screen>
     </Tab.Navigator>
-    </KeyboardAvoidingView>
     
   );
 }
